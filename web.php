7035cb419b9a0404046f648949e58f6a598252c8
mathis <138501804+dev-sio-M@users.noreply.github.com>
2024-03-07 15:28:24 +0100

    Update web.php

--- web.php
+++ web.php
@@ -21,9 +21,9 @@ Route::get('/', function () {
 });
 
 /*Visiteur médical*/
-Route::get('/visiteurMedical/v_connexion', 'App\Http\Controllers\c_connexion@demandeConnexion')->name('demandeConnexion');
+Route::get('/visiteurMedical/v_connexion', 'App\Http\Controllers\c_connexion@demandeConnexionVisiteur')->name('demandeConnexionVisiteur');
 
-Route::post('/visiteurMedical/v_connexion', 'App\Http\Controllers\c_connexion@valideConnexion')->name('valideConnexion');
+Route::post('/visiteurMedical/v_connexion', 'App\Http\Controllers\c_connexion@valideConnexionVisiteur')->name('valideConnexionVisiteur');
 
 Route::get('/visiteurMedical/v_sommaire', function () {
     return view('visiteurMedical.v_sommaire');
@@ -37,13 +37,13 @@ Route::get('/visiteurMedical/consulterFichesFrais', function () {
 
 
 //~ /*Comptable*/
-//~ Route::get('/comptable/v_connexionComptable', 'App\Http\Controllers\c_connexionComptable@demandeConnexion')->name('demandeConnexion');
+Route::get('/comptable/v_connexionComptable', 'App\Http\Controllers\c_connexionComptable@demandeConnexionComptable')->name('demandeConnexionComptable');
 
-//~ Route::post('/comptable/v_connexionComptable', 'App\Http\Controllers\c_connexionComptable@valideConnexion')->name('valideConnexion');
+Route::post('/comptable/v_connexionComptable', 'App\Http\Controllers\c_connexionComptable@valideConnexionComptable')->name('valideConnexionComptable');
 
-//~ Route::get('/comptable/v_sommaireComptable', function () {
-    //~ return view('comptable.v_sommaireComptable');
-//~ });
+Route::get('/comptable/v_sommaireComptable', function () {
+    return view('comptable.v_sommaireComptable');
+});
 
 Route::post('/logout', [c_connexion::class, 'logout'])->name('logout');
 
